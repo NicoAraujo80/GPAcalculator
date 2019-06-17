74e6cb09c773de07c46814b30836e89563a6b963
Nicolas Araujo <nicoaraujo2002@gmail.com>
2019-06-17 00:04:00 -0400

    testing responsive design

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,9 +1,9 @@
 <nav style="width: 100vw; height: 70px; background-color: #ff4227;">
     <div class="row">
-        <div class="col-md-6 offset-md-3 col-8">
-            <h1 style="text-align: center; margin: .4em;">GHS GPA Calculator</h1>
+        <div class="col-md-6 offset-md-3 col-7">
+            <h1 style="text-align: center; margin: .4em; font-size: 2.5em;">GHS GPA Calculator</h1>
         </div>
-        <div class="col-md-2 col-3">
+        <div class="col-md-2 col-4">
             <a href="{{ route('chooseClasses') }}" style="margin: 1em;" class="btn btn-success btn-block">Edit Classes</a>
         </div>
     </div>
